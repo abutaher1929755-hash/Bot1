14a6fd8c4bfb358af4b01d672bbf12c382c30d4e
abutaher1929755-hash <abutaher1929755@gmail.com>
2026-08-12 16:38:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1114,4 +1114,6 @@ if __name__ == '__main__':
     print("🔗 API: http://localhost:5000/api/create")
     print("👤 admin / admin123")
     print("=" * 50 + "\n")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
